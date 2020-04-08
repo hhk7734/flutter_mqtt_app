a86328f3e51dd2c02a8a3447ecea545483abb6a5
Hyeonki Hong <hhk7734@gmail.com>
2020-04-08 22:46:01 +0900

    RemoteControl: bloc: add constructor

--- lib/blocs/remote_control/remote_control_bloc.dart
+++ lib/blocs/remote_control/remote_control_bloc.dart
@@ -2,11 +2,19 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+import '../../repositories/mqtt/mqtt_repository.dart';
 
 part 'remote_control_event.dart';
 part 'remote_control_state.dart';
 
 class RemoteControlBloc extends Bloc<RemoteControlEvent, RemoteControlState> {
+  final MqttRepository _mqttRepository;
+
+  RemoteControlBloc({@required MqttRepository mqttRepository})
+      : _mqttRepository = mqttRepository;
+
   @override
   RemoteControlState get initialState => RemoteControlInitial();
 
