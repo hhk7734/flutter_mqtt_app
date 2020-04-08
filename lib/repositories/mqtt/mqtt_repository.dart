1166ede8a12da619464e103a9930a827c9b071c5
Hyeonki Hong <hhk7734@gmail.com>
2020-04-08 18:00:22 +0900

    Mqtt: repository: add connect

--- lib/repositories/mqtt/mqtt_repository.dart
+++ lib/repositories/mqtt/mqtt_repository.dart
@@ -12,4 +12,29 @@ class MqttRepository {
     client.logging(on: false);
     client.keepAlivePeriod = 60;
   }
+
+  get mqttConnectionState => client.connectionStatus.state;
+
+  Future<bool> connect() async {
+    assert(client.server != null);
+    assert(client.clientIdentifier != null);
+
+    switch (mqttConnectionState) {
+      case MqttConnectionState.disconnected:
+        try {
+          await client?.connect();
+        } on Exception catch (e) {
+          print('MqttRepository: connect: exception - $e');
+          client.disconnect();
+        }
+        break;
+    }
+
+    switch (mqttConnectionState) {
+      case MqttConnectionState.connected:
+        return true;
+      default:
+        return false;
+    }
+  }
 }
