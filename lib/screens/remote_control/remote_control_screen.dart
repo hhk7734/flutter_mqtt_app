4adc5dd3f7cfc6828b325dda04a8992d8cae972c
Hyeonki Hong <hhk7734@gmail.com>
2020-04-10 17:27:26 +0900

    RemoteControl: screen: implement UI

--- lib/screens/remote_control/remote_control_screen.dart
+++ lib/screens/remote_control/remote_control_screen.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'dart:convert';
 
 import '../../blocs/remote_control/remote_control_bloc.dart';
 
 import '../../repositories/mqtt/mqtt_repository.dart';
 
-class RemoteControlScreen extends StatefulWidget {
+class RemoteControlScreen extends StatelessWidget {
   static const String id = 'remote_control';
   final MqttRepository _mqttRepository;
 
@@ -14,39 +13,109 @@ class RemoteControlScreen extends StatefulWidget {
       : _mqttRepository = mqttRepository;
 
   @override
-  _RemoteControlScreenState createState() => _RemoteControlScreenState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => RemoteControlBloc(
+        mqttRepository: _mqttRepository,
+      ),
+      child: WillPopScope(
+        onWillPop: () {
+          print('RemoteControlScreen: onWillPop');
+          return Future.value(true);
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'ODROID-C4 + Flutter + Mqtt',
+              style: TextStyle(
+                color: Colors.indigo,
+              ),
+            ),
+            backgroundColor: Colors.lime[200],
+          ),
+          body: BlocBuilder<RemoteControlBloc, RemoteControlState>(
+            builder: (context, state) {
+              if (state is RemoteControlValueUpdateSuccess) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(
+                    state.values.length,
+                    (index) => ControlWidget(
+                      index: index,
+                      state: state,
+                    ),
+                  ),
+                );
+              } else if (state is RemoteControlUnsolicitedlyDisconnectSuccess) {
+                Future.delayed(Duration(seconds: 2))
+                    .then((value) => Navigator.of(context).pop());
+                return Center(child: Text('Unsolicitedly Disconnected.'));
+              } else {
+                BlocProvider.of<RemoteControlBloc>(context)
+                    .add(RemoteControlValueGeted(topic: 'get'));
+                return Center(child: Text('Initial'));
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _RemoteControlScreenState extends State<RemoteControlScreen> {
+class ControlWidget extends StatefulWidget {
+  final int index;
+  final RemoteControlValueUpdateSuccess state;
+
+  ControlWidget({
+    this.index,
+    this.state,
+  });
+
+  @override
+  _ControlWidgetState createState() => _ControlWidgetState();
+}
+
+class _ControlWidgetState extends State<ControlWidget> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Remote Control'),
-      ),
-      body: BlocProvider(
-        create: (context) => RemoteControlBloc(
-          mqttRepository: widget._mqttRepository,
-        ),
-        child: BlocBuilder<RemoteControlBloc, RemoteControlState>(
-          builder: (context, state) {
-            if (state is RemoteControlValueUpdateSuccess) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: List.generate(
-                  state.values.length,
-                  (index) {
-                    return Text('${state.values[index]} ');
-                  },
+    Color color =
+        widget.state.values[widget.index] ? Colors.red[200] : Colors.grey;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.fromLTRB(3, 1, 3, 0),
+          child: Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                  color: Colors.blue[100],
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration:
+                      BoxDecoration(color: color, shape: BoxShape.circle),
                 ),
-              );
-            } else {
-              return Text('Initial');
-            }
+              )),
+        ),
+        RaisedButton(
+          child: Text('Toggle'),
+          onPressed: () {
+            setState(
+              () {
+                BlocProvider.of<RemoteControlBloc>(context).add(
+                  RemoteControlValueSeted(
+                      currentValues: widget.state.values,
+                      index: widget.index,
+                      nextValue: widget.state.values[widget.index] ^= true),
+                );
+              },
+            );
           },
         ),
-      ),
+      ],
     );
   }
 }
